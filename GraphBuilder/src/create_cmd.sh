6bb3a874d56d429efd0bd65f2aba8c6625630a6b
Liang Ding <adamdingliang@gmail.com>
2017-10-02 10:41:18 -0500

    max_cycle_abundance_estimate

--- GraphBuilder/src/create_cmd.sh
+++ GraphBuilder/src/create_cmd.sh
@@ -7,7 +7,7 @@ do
         -n $prog_dir/inputs/relapse/edges_for_graph_S_full.txt \
         -s $prog_dir/inputs/relapse/edges_for_graph_S_segments.txt \
         -a $prog_dir/inputs//relapse/noCREST_copygain_LR3_details.txt \
-        -c $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_m${m}.txt \
-        -e $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_cover_m${2}.txt \
-        -u $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_abundance_m${m}.txt
+        -c $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycles_m${m}.txt \
+        -e $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_covers_m${m}.txt \
+        -u $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_abundances_m${m}.txt
 done
---
+++ GraphBuilder/src/sub_rela.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sub_array_for_cmdfile.sh /research/rgs01/home/clusterHome/lding/Git/circDNA/GraphBuilder/src/cmd_rela.sh --log-tpl /research/rgs01/home/clusterHome/lding/Git/circDNA/GraphBuilder/logs/cmd_rela_%W.%J.%I.txt

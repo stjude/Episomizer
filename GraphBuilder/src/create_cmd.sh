60f23a7f7e86bc75f5eadf108834a905adbac223
Liang Ding <adamdingliang@gmail.com>
2017-10-01 21:57:43 -0500

    updates

--- GraphBuilder/src/create_cmd.sh
+++ GraphBuilder/src/create_cmd.sh
@@ -4,7 +4,7 @@ prog_dir='/home/lding/Git/circDNA/GraphBuilder'
 for m in {2..20}
 do
     echo $prog_dir/src/circDNA.py circ -m $m \
-        -n $prog_dir/inputs/relapse/edges_for_graph_S_full_no_chromium.txt \
+        -n $prog_dir/inputs/relapse/edges_for_graph_S_full.txt \
         -s $prog_dir/inputs/relapse/edges_for_graph_S_segments.txt \
         -a $prog_dir/inputs//relapse/noCREST_copygain_LR3_details.txt \
         -c $prog_dir/outputs/relapse/max_cycle_abundance_estimate/cycle_m${m}.txt \

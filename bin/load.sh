68c51df4312d490f789846d20fe0faefe69ec4e0
adamdingliang <adamdingliang@gmail.com>
2018-08-29 20:51:57 -0500

    first commit

---
+++ bin/load.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Check for evidence of being sourced
+if [ "${BASH_SOURCE[0]}" == "$0" ]
+then
+  echo "Error: it appears script has not been sourced--it will have no effect." >&2
+  return 1
+  exit 1
+fi
+
+EPISOMIZER_HOME=$1
+# Check if EPISOMIZER_HOME has been set 
+if [ -z "$EPISOMIZER_HOME" ]; then
+    echo "Error: EPISOMIZER_HOME not set" >&2
+    return 1
+    exit 1
+fi
+
+# Add paths to environmental variables
+prepend() {
+  echo "$1" | tr : '\n' | awk -v new="`readlink -f $2`$3" 'BEGIN { print new } $0 != new { print }' | tr '\n' : | sed 's/:$//'
+}
+addbin() {
+  if [ -d $1 ]; then NEWPATH=$(prepend "$PATH" "$1"); export PATH="$NEWPATH"; fi
+}
+addpython() {
+  if [ -d $1 ]; then NEWPYTHONPATH=$(prepend "$PYTHONPATH" "$1"); export PYTHONPATH="$NEWPYTHONPATH"; fi
+}
+addperl() {
+  if [ -d $1 ]; then NEWPERL5LIB=$(prepend "$PERL5LIB" "$1"); export PERL5LIB="$NEWPERL5LIB"; fi
+}
+addpython $EPISOMIZER_HOME/src/composer
+addbin $EPISOMIZER_HOME/bin
+
+echo "Sourcing completed successfully"

de3f932b4934ee5487f1691120685808e968eb23
adamdingliang <adamdingliang@gmail.com>
2018-09-09 20:32:28 -0500

    Revert "update installation"
    This reverts commit 72c22b4b9e95382b737ca77e71627e301b010c69.

--- tests/test_circ.py
+++ tests/test_circ.py
@@ -11,36 +11,22 @@ class TestComposer(unittest.TestCase):
     def setUp(self):
         self.tool = 'composer.py circ'
         self.test_data_dir = './testdata/composer'
-        self.seg_file_relapse = '{}/inputs/relapse/CNA_region_raw_R.bed'.format(self.test_data_dir)
-        self.non_seg_file_relapse = '{}/inputs/relapse/Reviewed_edges_for_graph_relapse_' \
-                                    'rawCNA.txt'.format(self.test_data_dir)
-        self.cmd_input_relapse = '{} -c {} -l {}'.format(self.tool, self.seg_file_relapse, self.non_seg_file_relapse)
-        self.seg_file_diagnosis = '{}/inputs/diagnosis/Reviewed_segments_for_graph_diagnosis.' \
-                                  'bed'.format(self.test_data_dir)
-        self.non_seg_file_diagnosis = '{}/inputs/diagnosis/Reviewed_edges_for_graph_diagnosis' \
-                                      '_rawCNA.txt'.format(self.test_data_dir)
-        self.cmd_input_diagnosis = '{} -c {} -l {}'.format(self.tool, self.seg_file_diagnosis,
-                                                           self.non_seg_file_diagnosis)
+        self.seg_file = '{}/inputs/relapse/CNA_region_raw_R.bed'.format(self.test_data_dir)
+        self.non_seg_file = '{}/inputs/relapse/Reviewed_edges_for_graph_relapse_rawCNA.txt'.format(self.test_data_dir)
+        self.cmd_input = '{} -c {} -l {}'.format(self.tool, self.seg_file, self.non_seg_file)
 
     def test_graph_to_sif(self):
         """ Test networkx graph to sif function.
         """
         out_sif_file = '{}/outputs/relapse/graph.sif'.format(self.test_data_dir)
-        cmd = '{} -s {}'.format(self.cmd_input_relapse, out_sif_file)
+        cmd = '{} -s {}'.format(self.cmd_input, out_sif_file)
         util.run_shell_command_call(cmd)
 
-    def test_find_cycles_relapse(self):
-        """ Test finding all simple cycles for the relapse sample
+    def test_find_cycles(self):
+        """ Test finding all simple cycles.
         """
         out_cycle_file = '{}/outputs/relapse/double_minutes.txt'.format(self.test_data_dir)
-        cmd = '{} -d {}'.format(self.cmd_input_relapse, out_cycle_file)
-        util.run_shell_command_call(cmd)
-
-    def test_find_cycles_diagnosis(self):
-        """ Test finding all simple cycles for the relapse sample
-        """
-        out_cycle_file = '{}/outputs/diagnosis/double_minutes.txt'.format(self.test_data_dir)
-        cmd = '{} -d {}'.format(self.cmd_input_diagnosis, out_cycle_file)
+        cmd = '{} -d {}'.format(self.cmd_input, out_cycle_file)
         util.run_shell_command_call(cmd)
 
     def tearDown(self):
